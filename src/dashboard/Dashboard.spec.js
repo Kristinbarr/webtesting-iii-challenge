dfb7123ee24e56e59bd69087e7e3e71246367e95
Kristin Barr <kristinbarr@gmail.com>
2019-08-19 23:32:11 -0400

    added display snapshot test for dashboard

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,13 @@
-// Test away
+// shows the controls and display
+
+import React from 'react'
+import renderer from 'react-test-renderer'
+
+import Dashboard from './Dashboard'
+
+describe('<Dashboard />', () => {
+  it('matches snapshot', () => {
+    const tree = renderer.create(<Dashboard />)
+    expect(tree.toJSON()).toMatchSnapshot()
+  })
+})
